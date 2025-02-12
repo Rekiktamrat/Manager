ae12276c21a62c6ab744ff52dc958e1bf28d872a
Rekiktamrat <rekiktamrat72@gmail.com>
2025-02-12 11:22:57 +0300

    message

--- src/App.js
+++ src/App.js
@@ -22,14 +22,14 @@ const App = () => {
     <Router>
       <Routes>
         {/* ✅ Redirect root ("/") to login */}
-          <Route path="user-management" element={<UserManagement />} />
-          <Route path="property-management" element={<PropertyManagement />} />
         <Route path="/" element={<Navigate to="/login" replace />} />
 
         {/* Manager routes */}
         <Route path="/manager" element={<ManagerLayout />}>
           <Route path="dashboard" element={<Dashboard />} />
+            <Route path="property-management" element={<PropertyManagement />} />
           <Route path="rental-transactions" element={<RentalTransactions />} />
+            <Route path="user-management" element={<UserManagement />} />
           <Route path="sale-transactions" element={<SaleTransactions />} />
           <Route path="scheduling" element={<Scheduling />} />
           <Route path="profile" element={<Profile />} />
